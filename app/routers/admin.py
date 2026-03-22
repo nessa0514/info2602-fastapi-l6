76a377a8dfbcf8a021264df439afa1c08df9b5f3
nessa0514 <aa.fariah.ml@gmail.com>
2026-03-22 06:50:48 -0400

    task 4.1

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -16,11 +16,14 @@ from app.pagination import Pagination
 admin_router = APIRouter(tags=["Admin App"])
 
 @admin_router.get("/admin")
-def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(default=1, ge=1), limit: int = Query(default=100, le=100)):
+def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(default=1, ge=1), limit: int = Query(default=100, le=100), q: str = Query(default='')):
     offset = (page - 1) * limit
 
     count_todos = db.exec(select(func.count(Todo.id))).one()
-    todos = db.exec(select(Todo).offset(offset).limit(limit)).all()
+    if q:
+        todos = db.exec(select(Todo).join(User).where(Todo.text.ilike(f"%{q}%") | User.username.ilike(f"%{q}%")).offset(offset).limit(limit)).all()
+    else:
+        todos = db.exec(select(Todo).offset(offset).limit(limit)).all()
     pagination = Pagination(total_count=count_todos, current_page=page, limit=limit)
 
     return templates.TemplateResponse(
@@ -30,5 +33,6 @@ def admin_page(request:Request, db:SessionDep, user:AdminDep, page: int = Query(
             "current_user": user,
             "todos": todos,
             "pagination": pagination,
+            "q":q
         }
     )
